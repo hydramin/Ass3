e37b35cf0b42e2db17f9b0ad68a051ebab0f0014
hydramin <genetamin@gmail.com>
2018-03-22 10:34:26 -0400

    practice folder updated, it is used as a sample of semaphore use

--- assign3/alarm_cond.c
+++ assign3/alarm_cond.c
@@ -12,10 +12,7 @@
  */
 #include <pthread.h>
 #include <time.h>
-#include<semaphore.h>
 #include "errors.h"
-
- #define DEBUG  1 // UNCOMMENT TO ALLOW DEBUGGIN PRINT
 
 /*
  * The "alarm" structure now contains the time_t (time since the
@@ -24,24 +21,15 @@
  * enough, since the "alarm thread" cannot tell how long it has
  * been on the list.
  */
-typedef struct alarm_tag {    
+typedef struct alarm_tag {
     struct alarm_tag    *link;
     int                 seconds;
-    time_t              time;           /*seconds from EPOCH*/
-    char                message[128];
-    int                 type;           /*Message type*/
-    int                 number;           /*Message number*/
-    /*int                 isAssigned;     // whether the alarm is assigned to a thread or not (is 1 or 0)*/
-
+    time_t              time;   /* seconds from EPOCH */
+    char                message[64];
 } alarm_t;
 
-sem_t reader_count_mutex;
-sem_t write_mutex;
-int reader_count=0;
-
-void *Reader(void *arg);
-void *Writer(void *arg);
-
+pthread_mutex_t alarm_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t alarm_cond = PTHREAD_COND_INITIALIZER;
 alarm_t *alarm_list = NULL;
 time_t current_alarm = 0;
 
@@ -62,7 +50,7 @@ void alarm_insert (alarm_t *alarm)
     last = &alarm_list;
     next = *last;
     while (next != NULL) {
-        if (next->number >= alarm->number) {
+        if (next->time >= alarm->time) {
             alarm->link = next;
             *last = alarm;
             break;
@@ -82,184 +70,173 @@ void alarm_insert (alarm_t *alarm)
 #ifdef DEBUG
     printf ("[list: ");
     for (next = alarm_list; next != NULL; next = next->link)
-        // printf ("%d(%d)[\"%s\"] ", next->time,next->time - time (NULL), next->message);
-        printf ("Num-> %d ; Type-> %d ; Time-> %d ; Msg-> %s \n", next->number,next->type,next->time - time (NULL), next->message);
-    // printf ("]\n");
+        printf ("%d(%d)[\"%s\"] ", next->time,
+            next->time - time (NULL), next->message);
+    printf ("]\n");
 #endif
-
+    /*
+     * Wake the alarm thread if it is not busy (that is, if
+     * current_alarm is 0, signifying that it's waiting for
+     * work), or if the new alarm comes before the one on
+     * which the alarm thread is waiting.
+     */
+    if (current_alarm == 0 || alarm->time < current_alarm) {
+        current_alarm = alarm->time;
+        status = pthread_cond_signal (&alarm_cond);
+        if (status != 0)
+            err_abort (status, "Signal cond");
+    }
 }
 
-// <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> ALARM THREAD FUNCTION
 /*
  * The alarm thread's start routine.
  */
-// void *alarm_thread (void *arg)
-// {
-//     alarm_t *alarm;    
-//     time_t now;
-//     int status, expired;
+void *alarm_thread (void *arg)
+{
+    alarm_t *alarm;
+    struct timespec cond_time;
+    time_t now;
+    int status, expired;
 
-//     /*
-//      * Loop forever, processing commands. The alarm thread will
-//      * be disintegrated when the process exits. Lock the mutex
-//      * at the start -- it will be unlocked during condition
-//      * waits, so the main thread can insert alarms.
-//      */
-//     status = pthread_mutex_lock (&alarm_mutex);
-//     if (status != 0)
-//         err_abort (status, "Lock mutex");
-//     while (1) {
-//         /*
-//          * If the alarm list is empty, wait until an alarm is
-//          * added. Setting current_alarm to 0 informs the insert
-//          * routine that the thread is not busy.
-//          */
-//         current_alarm = 0;
-//         while (alarm_list == NULL) {
-//             status = pthread_cond_wait (&alarm_cond, &alarm_mutex);
-//             if (status != 0)
-//                 err_abort (status, "Wait on cond");
-//             }
-//         alarm = alarm_list;
-//         alarm_list = alarm->link;
-//         now = time (NULL);
-//         expired = 0;
-//         if (alarm->time > now) {
-// #ifdef DEBUG
-//             printf ("[waiting: %d(%d)\"%s\"]\n", alarm->time,
-//                 alarm->time - time (NULL), alarm->message);
-// #endif
-//             cond_time.tv_sec = alarm->time;
-//             cond_time.tv_nsec = 0;
-//             current_alarm = alarm->time;
-//             while (current_alarm == alarm->time) {
-//                 status = pthread_cond_timedwait (
-//                     &alarm_cond, &alarm_mutex, &cond_time);
-//                 if (status == ETIMEDOUT) {
-//                     expired = 1;
-//                     break;
-//                 }
-//                 if (status != 0)
-//                     err_abort (status, "Cond timedwait");
-//             }
-//             if (!expired)
-//                 alarm_insert (alarm);
-//         } else
-//             expired = 1;
-//         if (expired) {
-//             printf ("(%d) %s\n", alarm->seconds, alarm->message);
-//             free (alarm);
-//         }
-//     }
-// }
+    /*
+     * Loop forever, processing commands. The alarm thread will
+     * be disintegrated when the process exits. Lock the mutex
+     * at the start -- it will be unlocked during condition
+     * waits, so the main thread can insert alarms.
+     */
+    status = pthread_mutex_lock (&alarm_mutex);
+    if (status != 0)
+        err_abort (status, "Lock mutex");
+    while (1) {
+        /*
+         * If the alarm list is empty, wait until an alarm is
+         * added. Setting current_alarm to 0 informs the insert
+         * routine that the thread is not busy.
+         */
+        current_alarm = 0;
+        while (alarm_list == NULL) {
+            status = pthread_cond_wait (&alarm_cond, &alarm_mutex);
+            if (status != 0)
+                err_abort (status, "Wait on cond");
+            }
+        alarm = alarm_list;
+        alarm_list = alarm->link;
+        now = time (NULL);
+        expired = 0;
+        if (alarm->time > now) {
+#ifdef DEBUG
+            printf ("[waiting: %d(%d)\"%s\"]\n", alarm->time,
+                alarm->time - time (NULL), alarm->message);
+#endif
+            cond_time.tv_sec = alarm->time;
+            cond_time.tv_nsec = 0;
+            current_alarm = alarm->time;
+            while (current_alarm == alarm->time) {
+                status = pthread_cond_timedwait (
+                    &alarm_cond, &alarm_mutex, &cond_time);
+                if (status == ETIMEDOUT) {
+                    expired = 1;
+                    break;
+                }
+                if (status != 0)
+                    err_abort (status, "Cond timedwait");
+            }
+            if (!expired)
+                alarm_insert (alarm);
+        } else
+            expired = 1;
+        if (expired) {
+            printf ("(%d) %s\n", alarm->seconds, alarm->message);
+            free (alarm);
+        }
+    }
+}
 
-// <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> MAIN METHOD
 int main (int argc, char *argv[])
-{    
-    /*Initialize semaphores*/
-    sem_init(&reader_count_mutex,0,1);
-    sem_init(&write_mutex,0,1);
-
+{
     int status;
     char line[1000];
     alarm_t *alarm;
     pthread_t thread;
 
-    /*status = pthread_create (&thread, NULL, alarm_thread, NULL);
+    status = pthread_create (&thread, NULL, alarm_thread, NULL);
     if (status != 0)
-        err_abort (status, "Create alarm thread");*/
+        err_abort (status, "Create alarm thread");
 
     while (1) {
         printf ("Alarm> ");
         if (fgets (line, sizeof (line), stdin) == NULL) exit (0);
         if (strlen (line) <= 1) continue;
-        
 
+        alarm = (alarm_t*)malloc (sizeof (alarm_t));
+        if (alarm == NULL)
+            errno_abort ("Allocate alarm");
 // <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> INPUT PARSING BLOCK
         /*
          * Parse input line into seconds (%d) and a message
-         * (%64[^\n]), consisting of up to 64 characters
+         * (%128[^\n]), consisting of up to 64 characters
          * separated from the seconds by whitespace.
 
          Alarm> Time Message(Message_Type, Message_Number) Message
          Alarm> Create_Thread: MessageType(Message_Type)
          Alarm> Cancel: Message(Message_Number)
-
          */
-        int err_t1, err_t2, err_t3;
-        int t1_sec, t1_type, t1_num;
-        char t1_msg[128];
-        int t2_type, t3_num;
-        /*parse the input string into the componenents required and use the if 
-        block below to check validity of the values entered*/
-        err_t1 = sscanf(line,"%d Message(%d, %d) %128[^\n]",&t1_sec,&t1_type,&t1_num,t1_msg);
-        err_t2 = sscanf(line, "Create_Thread: MessageType(%d)",&t2_type);
-        err_t3 = sscanf(line, "Cancel: Message(%d)",&t3_num);
+         int err_t1, err_t2, err_t3;
+         int t1_sec, t1_type, t1_num;
+         char t1_msg[128];
+         int t2_type, t3_num;
+
+         err_t1 = sscanf(line,"%d Message(%d, %d) %128[^\n]",&t1_sec,&t1_type,&t1_num,t1_msg);
+         err_t2 = sscanf(line, "Create_Thread: MessageType(%d)",&t2_type);
+         err_t3 = sscanf(line, "Cancle: Message(%d)",&t3_num);
 // <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> INPUT VALIDATION BLOCK
-        /*Check if there is any error in the commands entered, 
+
+         /*Check if there is any error in the commands entered, 
         if error exists, restart the loop*/        
         if (err_t1 < 4 && err_t2 < 1 && err_t3 < 1) {            
-            printf("Bad Command. Usage: \nType A: <+ve integer> Message(<+ve integer>, <+ve integer>) <string message> \nType B: Create_Thread: MessageType(<+ve integer>) \nType C: Calcel: Message(<+ve integer>)\n");
+            printf("Bad Command. Usage: \nType A: <+ve integer> MessageType(<+ve integer>) <string message> \nType B: Create_Thread: MessageType(<+ve integer>) \nType C: Terminate_Thread: MessageType(<+ve integer>)\n");
             continue;
         } 
         /*if alarm request command, thread creation and thread termination commands are
          correct, check if the seconds and/or type of message have non negative values,
           if negative values entered display error message and restart the loop*/
-        if(err_t1 == 4){            
-            if (t1_sec <= 0 || t1_type < 0 || t1_num < 0){
-                printf("Bad Command. Usage: \nType A: <+ve integer> Message(<+ve integer>, <+ve integer>) <string message> \nType B: Create_Thread: MessageType(<+ve integer>) \nType C: Calcel: Message(<+ve integer>)\n");
+        if(err_t1 == 3){            
+            if (t1_sec <= 0 || t1_type < 0){
+                printf("Bad Command. Usage: \nType A: <+ve integer> MessageType(<+ve integer>) <string message> \nType B: Create_Thread: MessageType(<+ve integer>) \nType C: Terminate_Thread: MessageType(<+ve integer>)\n");
                 continue;
             }
         }
         else if(err_t2 == 1){
             if (t2_type <= 0){
-                printf("Bad Command. Usage: \nType A: <+ve integer> Message(<+ve integer>, <+ve integer>) <string message> \nType B: Create_Thread: MessageType(<+ve integer>) \nType C: Calcel: Message(<+ve integer>)\n");
+                printf("Bad Command. Usage: \nType A: <+ve integer> MessageType(<+ve integer>) <string message> \nType B: Create_Thread: MessageType(<+ve integer>) \nType C: Terminate_Thread: MessageType(<+ve integer>)\n");
                 continue;
             }
         }
         else if(err_t3 == 1){
-            if (t3_num <= 0){                
-                printf("Bad Command. Usage: \nType A: <+ve integer> Message(<+ve integer>, <+ve integer>) <string message> \nType B: Create_Thread: MessageType(<+ve integer>) \nType C: Calcel: Message(<+ve integer>)\n");
+            if (t3_type <= 0){                
+                printf("Bad Command. Usage: \nType A: <+ve integer> MessageType(<+ve integer>) <string message> \nType B: Create_Thread: MessageType(<+ve integer>) \nType C: Terminate_Thread: MessageType(<+ve integer>)\n");
                 continue;
             }
         }
-
-// <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> ALARM SETTING/INPUT BLOCK
-/*1==>*/if(err_t1 == 4){
-
-            alarm = (alarm_t*)malloc (sizeof (alarm_t));             
-            if (alarm == NULL)
-                errno_abort ("Allocate alarm");                    
-
-            /*parse a Type A command and assign the element of the alarm*/
-            if (sscanf(line,"%d Message(%d, %d) %128[^\n]", &alarm->seconds,&alarm->type,&alarm->number,alarm->message) < 4){        
-                fprintf (stderr, "Bad command, Not Type A\n");
-                free (alarm);
-                continue;
-            } else {
-                /*set the Type A alarm and put it in the list
-                create the alarm and pass it to be added into the
-                alarm_list*/
-                alarm->time = time(NULL) + alarm->seconds;
-                
-                status = sem_wait(&write_mutex);
-                if (status != 0)
-                    err_abort (status, "Lock mutex");
-                
-                /*
-                * Insert the new alarm into the list of alarms,
-                * sorted by message type.
-                */
-                alarm_insert (alarm);
-                status = sem_post(&write_mutex);
-                if (status != 0)
-                    err_abort (status, "Unlock mutex");
-            }     
-
-        } else if (err_t2 == 1){
-
-        } else if(err_t3 == 1){
-
+        
+// <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> ALARM SETTING/INPUT BLOCK        
+        if (sscanf (line, "%d %64[^\n]", 
+            &alarm->seconds, alarm->message) < 2) {
+            fprintf (stderr, "Bad command\n");
+            free (alarm);
+        } else {
+            status = pthread_mutex_lock (&alarm_mutex);
+            if (status != 0)
+                err_abort (status, "Lock mutex");
+            alarm->time = time (NULL) + alarm->seconds;
+            /*
+             * Insert the new alarm into the list of alarms,
+             * sorted by expiration time.
+             */
+            alarm_insert (alarm);
+            status = pthread_mutex_unlock (&alarm_mutex);
+            if (status != 0)
+                err_abort (status, "Unlock mutex");
         }
     }
 }
--- assign3/practice/rw_problem.c
+++ assign3/practice/rw_problem.c
@@ -11,27 +11,31 @@ void *Writer(void *arg);
 
 int main()
 {
-    int i=0,NumberofReaderThread=0,NumberofWriterThread;
+    int i=0,NumberofReaderThread=0,NumberofWriterThread=0;
+    /*initialize the semaphores. readCountAccess is access to update the number
+    readers currently*/
     sem_init(&readCountAccess,0,1);
     sem_init(&databaseAccess,0,1);
 
+    /*allow a 100 reader and writer threads but let the user decide the number of
+    threads to create of each type*/
     pthread_t Readers_thr[100],Writer_thr[100];
     printf("\nEnter number of Readers thread(MAX 10)");
     scanf("%d",&NumberofReaderThread);
     printf("\nEnter number of Writers thread(MAX 10)");
     scanf("%d",&NumberofWriterThread);
 
-    /*create the writer and reader threads*/
-    for(i=0; i<NumberofReaderThread; i++)
+    /*loop and create the reader threads*/
+    for(i=0; i<numberofreaderthread; i++)
     {
         pthread_create(&Readers_thr[i],NULL,Reader,(void *)i);
     }
-    for(i=0; i<NumberofWriterThread; i++)
+    /*loop and create all writer threads*/
+    for(i=0; i<numberofwriterthread; i++)
     {
         pthread_create(&Writer_thr[i],NULL,Writer,(void *)i);
     }
-
-    /*wait for the threads to finish*/
+    /*wait for the reader and writer threads to finish their work*/
     for(i=0; i<NumberofWriterThread; i++)
     {
         pthread_join(Writer_thr[i],NULL);
@@ -41,7 +45,7 @@ int main()
     {
         pthread_join(Readers_thr[i],NULL);
     }
-    /*destroy the semaphores*/
+    /*destroy the semaphores used*/
     sem_destroy(&databaseAccess);
     sem_destroy(&readCountAccess);
     return 0;
@@ -49,7 +53,6 @@ int main()
 
 void * Writer(void *arg)
 {
-
     sleep(1);
     int temp=(int)arg;
     printf("\nWriter %d is trying to enter into database for modifying the data",temp);
@@ -64,17 +67,21 @@ void *Reader(void *arg)
     sleep(1);
     int temp=(int)arg;
     printf("\nReader %d is trying to enter into the Database for reading the data",temp);
-    
+    /*increment readCount everytime a reader access the database then
+    the first reader thread blocks any writer from using the shared
+    memory*/
     sem_wait(&readCountAccess);
     readCount++;
     if(readCount==1)
     {
         sem_wait(&databaseAccess);
-        printf("\nReader %d is reading the database",temp);
     }
     sem_post(&readCountAccess);
+
+    printf("\nReader %d is reading the database",temp);
     sem_wait(&readCountAccess);
     readCount--;
+    /*the last thread releases the database for writting*/
     if(readCount==0)
     {
         printf("\nReader %d is leaving the database",temp);
